1507f6dc62ebde4f187b41ba1459c6243afb6d6b
Asmita-bit <asmi.gupta071@gmail.com>
2025-07-13 13:45:19 +0545

    tag managed in prod

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -7,6 +7,7 @@ module "instance_template" {
   ami_id        = "ami-05ffe3c48a9991133" # Example AMI
   instance_type = "t2.micro"
   project       = "hello world"
+  tags          = local.common_tags
 }
 
 module "s3_storage" {
