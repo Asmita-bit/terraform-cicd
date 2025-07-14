230bb02aefe9f07a1cd73b67054675e21fa9d7f5
Asmita <asmi.gupta071@gmail.com>
2025-07-14 08:50:17 +0000

    http_tokens state set to required

--- ec2-project/main.tf
+++ ec2-project/main.tf
@@ -2,12 +2,11 @@ resource "aws_instance" "asmita_ec2" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   associate_public_ip_address = true
-  # This is to force metadata v2, which mitigates role credential leakage in the event of a SSRF
-	metadata_options {
-		http_tokens = "required"
-	}
   tags = {
     Name    = var.Name
     Creator = var.Creator
   }
+  metadata_options {
+		http_tokens               = "required"
+	}
 }
