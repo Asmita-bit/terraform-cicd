53022de81058f9a1db50ce60e39e8bea4fba633e
Asmita <asmi.gupta071@gmail.com>
2025-07-14 09:35:21 +0000

    policy role added

--- ec2-project/main.tf
+++ ec2-project/main.tf
@@ -10,3 +10,65 @@ resource "aws_instance" "asmita_ec2" {
 		http_tokens               = "required"
 	}
 }
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "this"
+  path        = "/"
+  description = "ec2 policy to access s3"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+        ]
+        Effect   = "Allow"
+        Resource = [
+          "arn:aws:s3:::asmita-bucket-847984534",
+          "arn:aws:s3:::asmitagupta-terraform-state-bucket"
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "test_profile"
+  role =  aws_iam_role.ec2_role.name
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
--- s3-project/main.tf
+++ s3-project/main.tf
@@ -12,3 +12,43 @@ resource "aws_s3_bucket_versioning" "versioning" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket" "this" {
+  bucket = "example-restricted-bucket-1234"
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = data.terraform_remote_state.ec2_project.outputs.ec2_instance_arn
+        }
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+        ]
+        Resource = "${aws_s3_bucket.this.arn}/*"
+      }
+    ]
+  })
+}
+
+# Fetch the remote state from the EC2 project
+data "terraform_remote_state" "ec2_project" {
+  backend = "s3"
+  config = {
+    bucket = "remote-state-bucket-name"
+    key    = "ec2-project/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# Assume the EC2 project outputs the ARN as "ec2_instance_arn"
+output "remote_ec2_arn" {
+  value = data.terraform_remote_state.ec2_project.outputs.ec2_instance_arn
+}
