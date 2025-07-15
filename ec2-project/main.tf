bcb819f7b778fb909bea34fedb4b0fb481d04c56
Asmita-bit <asmi.gupta071@gmail.com>
2025-07-15 13:21:57 +0545

    attached policy to role

--- ec2-project/main.tf
+++ ec2-project/main.tf
@@ -60,16 +60,8 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   role =  aws_iam_role.ec2_role.name
 }
 
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.policy.arn
 }
 
